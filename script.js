aad44e74dcbce69a816c6ad70ef60eee8c55b2b8
v1ctorh <93092965+v1ctorh@users.noreply.github.com>
2021-11-29 23:41:34 -0300

    29.11.21
    comeback video!

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-function close(){
-    let closePopUp = document.getElementById("videoPopUp");
-    closePopUp.style.display = "none";
-    video.muted = true;
+function close() {
+  let closePopUp = document.getElementById("videoPopUp");
+  closePopUp.style.display = "none";
+  video.muted = true;
 }
-document.getElementById("close").addEventListener("click", close)
+document.getElementById("close").addEventListener("click", close);
 
-function show(){
-    let showPopUp = document.getElementById("videoPopUp");
-    showPopUp.style.display = "inline-block";
-    video.muted = false;
+function show() {
+  let showPopUp = document.getElementById("videoPopUp");
+  showPopUp.style.display = "inline-block";
+  video.muted = false;
 }
 document.getElementById("previewBtn").addEventListener("click", show);
 
-let video = document.getElementById("video1")
+let video = document.getElementById("video1");
 
-function play(){
-    video.play();
+function play() {
+  video.play();
 }
 document.getElementById("play").addEventListener("click", play);
 
-
-function pause(){
-    video.pause();
+function pause() {
+  video.pause();
 }
 document.getElementById("pause").addEventListener("click", pause);
 
-
-function volume(){
-    video.muted = false;
-    }
+function volume() {
+  video.muted = false;
+}
 document.getElementById("volume").addEventListener("click", volume);
+
+let controls = document.getElementById("videoControls")
+
+function esconder() {
+    controls.style.visibility = 'hidden';
+}
+function aparecer() {
+    controls.style.visibility = 'visible';
+}
